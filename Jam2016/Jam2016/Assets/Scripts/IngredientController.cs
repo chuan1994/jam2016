d2ba764844e2662808e8da618b21f96fcd9bccca
Sang Kim <noriqwe@gmail.com>
2016-01-30 12:23:12 +1300

    finished ingredient controller, making pentagon controller

---
+++ Jam2016/Jam2016/Assets/Scripts/IngredientController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IngredientController : MonoBehaviour {
+
+    public delegate void IngredientMouseHandler(GameObject g);
+    public event IngredientMouseHandler JarHandler;
+
+    [SerializeField]
+    float Speed;
+
+    [SerializeField]
+    private bool move;
+
+	// Use this for initialization
+	void Start () {
+        move = false;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*if (move) {
+            Vector3 target = new Vector3(0, 0, 0);
+            if (transform.position != target) {
+                float step = Speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, target, step);
+            }
+            else {
+                move = false;
+            }
+        }*/
+    }
+
+    void OnMouseDown()
+    {
+        if (JarHandler != null)
+        {
+            JarHandler(this.gameObject);
+        }
+
+        Destroy(this.gameObject);
+    }
+}
---
+++ Jam2016/Jam2016/Assets/Scripts/PentagonController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PentagonController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnEnable()
+    {
+        IngredientController.JarHandler += JarHandler;
+    }
+}
