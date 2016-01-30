8e8e0754d15ea074229efcbcf8cf6236ef130c9a
Sang Kim <noriqwe@gmail.com>
2016-01-30 13:16:56 +1300

    if two ingredients are put in pentagon, outputs another ingredient

--- Jam2016/Jam2016/Assets/Scripts/PentagonController.cs
+++ Jam2016/Jam2016/Assets/Scripts/PentagonController.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PentagonController : MonoBehaviour {
+
+    public delegate void PentagonHandler(GameObject g);
+    public static event PentagonHandler PentHandler;
 
     [SerializeField]
     float Speed;
 
+    [SerializeField]
+    List<GameObject> IngreList;
+
+    [SerializeField]
+    List<GameObject> OutIngre;
+
     GameObject Ingredient;
+    Vector3 OriginalPos;
 
     private bool move;
 
 	// Use this for initialization
 	void Start () {
-        OnEnable();
         move = false;
 	}
 	
@@ -27,15 +37,37 @@ public class PentagonController : MonoBehaviour {
             }
             else
             {
+                Instantiate(Ingredient, OriginalPos, Quaternion.Euler(new Vector3(0, 0, 0)));
                 move = false;
             }
+        } 
+        else
+        {
+            Destroy(Ingredient);
         }
+
+
+        if (IngreList.Count == 2)
+        {
+            if (!move)
+            {
+                Instantiate(OutIngre[0], new Vector3(10, -2, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+                for (int i = 0; i < 1; i++)
+                {
+                    IngreList.Remove(IngreList[i]);
+                }
+            }
+            
+        }
+
     }
 
     void JarHandler(GameObject g)
     {
+        IngreList.Add(g);
         Ingredient = g;
         move = true;
+        OriginalPos = Ingredient.transform.position;
     }
 
     void OnEnable()
@@ -46,5 +78,20 @@ public class PentagonController : MonoBehaviour {
     void OnDisable()
     {
         IngredientController.JarHandler -= JarHandler;
+    }
+
+    void CombinationCheck(List<GameObject> i)
+    {
+        if (i[0].name == "Eyeball")
+        {
+            if (i[1].name == "Eyeball") {
+                if (PentHandler != null)
+                {
+                    PentHandler(OutIngre[0]);
+                    Instantiate(OutIngre[0], new Vector3(10, -2, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+                }
+            }
+            
+        }
     }
 }
