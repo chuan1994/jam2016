0c8bd229f91dabb4a09291b02bb5012044092e0e
Andy Choi <cybreus001@gmail.com>
2016-01-31 09:26:13 +1300

    main menu made

---
+++ Jam2016/Jam2016/Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    bool moving = false;
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    Vector3 GamePlayCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (moving)
+        {
+            if (transform.position != GamePlayCamera)
+            {
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, GamePlayCamera, step);
+            }
+            else
+            {
+                moving = false;
+            }
+        }
+	}
+
+    public void GameStart() {
+        moving = true;
+    }
+}
---
+++ Jam2016/Jam2016/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GameStart()
+    {
+        StartCoroutine(fadeOutMenu());
+    }
+
+    IEnumerator fadeOutMenu()
+    {
+        while (gameObject.GetComponent<CanvasGroup>().alpha != 0)
+        {
+            gameObject.GetComponent<CanvasGroup>().alpha = gameObject.GetComponent<CanvasGroup>().alpha - 0.1f;
+            yield return new WaitForSeconds(0.1f);
+        }
+        gameObject.SetActive(false);
+    }
+
+    public void showMenu()
+    {
+        gameObject.GetComponent<CanvasGroup>().alpha = 1f;
+        gameObject.SetActive(true);
+    }
+}
